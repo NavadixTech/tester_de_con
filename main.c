03e083333d94bad176f4072c368d70625b9e88a3
Lamour Sacha <sacha.lamour@epitech.eu>
2025-04-04 10:30:21 +0200

    TEST

--- main.c
+++ main.c
@@ -8,10 +8,8 @@
 #include <stdio.h>
 #include <stdalign.h>
 
-int main(void) {
-
-
-
-    printf("Hello, Chocolatine CI!\n                                                                                        ");
+int main(void)
+{
+    printf("Hello, Chocolatine CI!\n");
     return 0;
 }
