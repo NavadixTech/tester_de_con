ee333fd7ba29f5c84c5a1549d89a7063a3f6fd37
Lamour Sacha <sacha.lamour@epitech.eu>
2025-04-04 17:32:16 +0200

    [CHECK codingstyle

--- main.c
+++ main.c
@@ -1,14 +1,7 @@
-/*
-** EPITECH PROJECT, 2025
-** tester_de_con
-** File description:
-** main
-*/
 
 #include <stdio.h>
 
-int main(void)
-{
+int main(void){
     printf("Hello, this is my_radar!\n");
     return 0;
 }
